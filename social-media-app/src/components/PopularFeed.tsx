461a92eca2d501e660e325bdd5c57a8268845a77
shekhar <shekhar.devanshee@gmail.com>
2025-01-02 14:48:53 +0530

    popularpost dynamic data

--- social-media-app/src/components/PopularFeed.tsx
+++ social-media-app/src/components/PopularFeed.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./style.css";
+import "../style/style.css";
 interface NewsItem {
   title: string;
   description: string;
